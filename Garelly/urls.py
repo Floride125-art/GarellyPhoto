e9c04efb0627d7d636374454ee30d22ea0598e24
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-13 00:22:29 +0200

    modify my project to add models and url configurations

--- Garelly/urls.py
+++ Garelly/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^Photo/',include('Photo.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Photo/models.py
+++ Photo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db import models
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
 
 # Create your models here.
